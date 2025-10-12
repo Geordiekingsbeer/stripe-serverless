c89e006e5c14f1254f29b3fcbddf6bd1d2e47807
Geordiekingsbeer <88563547+Geordiekingsbeer@users.noreply.github.com>
2025-10-12 12:09:14 +0100

    Update and rename cors-preflight.js to cors-preflight.mjs

--- api/cors-preflight.mjs
+++ api/cors-preflight.mjs
@@ -1,5 +1,4 @@
 export default function (req, res) {
   // Returns 204 No Content for the successful CORS preflight check.
-  // Vercel will attach the necessary Access-Control-Allow-* headers from vercel.json.
   return res.status(204).end();
 }
